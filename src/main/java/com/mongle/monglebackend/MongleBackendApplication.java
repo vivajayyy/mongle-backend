8db0e6f966f88b87f65437a861dffe06837d887d
vivajayyy <vivajayyy@gmail.com>
2025-04-22 23:24:35 +0900

    init: 프로젝트 초기 세팅 및 구조 구성

---
+++ src/main/java/com/mongle/monglebackend/MongleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mongle.monglebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mongle/monglebackend/MongleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongle.monglebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
